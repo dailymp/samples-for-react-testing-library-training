2ccd1bf7d52ff622ac75b35046341703b600ded4
ramon.w.rocha <ramon.w.rocha@gmail.com>
2020-04-04 11:50:33 +0200

    init redux

---
+++ redux-app/src/components/Lista.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Lista extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+        return ( <div>Lista</div> );
+    }
+}
