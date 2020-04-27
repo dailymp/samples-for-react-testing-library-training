a770d9586e3eac90efd3464d29c8b329a50b7258
Mathew Muñoz <mathew.munoz@blue-tc.com>
2020-04-27 10:36:04 +0100

    issue#19: Add onMouseUp and onMouseDown tests

--- 03 Tests samples/src/components/MessageList/MessageList.test.tsx
+++ 03 Tests samples/src/components/MessageList/MessageList.test.tsx
@@ -1,70 +1,67 @@
 import * as React from 'react';
-import  { shallow, mount }  from 'enzyme';
+import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { act } from 'react-dom/test-utils';
-import  {MessageList} from './MessageList';
+import { MessageList } from './MessageList';
 
 describe('Message List component test', () => {
   configure({ adapter: new Adapter() });
 
-	let component;
-	const setState = jest.fn();
-	const useStateMock: any = (init) => [init, setState];
-    
-	beforeEach(() => {
-		component = shallow(< MessageList messages={[]}/>);
-	});
+  let component;
+  const messages = [
+    {
+      id: 1,
+      subject: 'Hello world',
+      body: 'Hello world',
+    },
+  ];
 
-	afterEach(() => {
+  beforeEach(() => {
+    component = shallow(<MessageList messages={[]} />);
+  });
+
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
-	it('should render a table', () =>{
-		expect(component.find('table')).toHaveLength(1);
-	});
+  it('should render a table', () => {
+    expect(component.find('table')).toHaveLength(1);
+  });
 
-	it('should render two  th', () =>{
-		expect(component.find('th')).toHaveLength(2);
-	});
+  it('should render two  th', () => {
+    expect(component.find('th')).toHaveLength(2);
+  });
 
-	it('should render  a thead', () =>{
-		expect(component.find('thead')).toHaveLength(1);
-	});
+  it('should render  a thead', () => {
+    expect(component.find('thead')).toHaveLength(1);
+  });
 
-	it('should render a tbody', () =>{
-		expect(component.find('tbody')).toHaveLength(1);
-	});
+  it('should render a tbody', () => {
+    expect(component.find('tbody')).toHaveLength(1);
+  });
 
-	it('should render none td', () =>{
-		expect(component.find('td')).toHaveLength(0);
-	});
+  it('should render none td', () => {
+    expect(component.find('td')).toHaveLength(0);
+  });
 
-	it('should render two td', () => {
-		let messages = [
-			{
-				id: 1,
-				subject: 'Hello world',
-				body: 'Hello world'
-			}
-		];
-		component = shallow(< MessageList messages={messages}/>);
-		expect(component.find('td')).toHaveLength(2);
-	});
+  it('should render two td', () => {
+    component = shallow(<MessageList messages={messages} />);
+    expect(component.find('td')).toHaveLength(2);
+  });
 
-	it('should set color red on mouse down on the tr', () => {
-		jest.spyOn(React, 'useState').mockImplementation(useStateMock);
-		let messages = [
-			{
-				id: 1,
-				subject: 'Hello world',
-				body: 'Hello world'
-			}
-		];
-		component = shallow(< MessageList messages={messages}/>);
-		// const bodyMessages = component.find('tr').at(1);
-		// bodyMessages.simulate('mousedown', { type: "mousedown" });
-		expect(setState).toHaveBeenCalledTimes(1);
-		// expect(bodyMessages.props().style).toHaveProperty('backgroundColor', 'blue');
-	});
+  it('should alert mousedown on mouse down on the tr', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    component = shallow(<MessageList messages={messages} />);
+    const bodyMessages = component.find('tr').at(1);
+    bodyMessages.simulate('mousedown', { type: 'mousedown' });
+    expect(window.alert).toBeCalledWith('mousedown');
+  });
+  
+  it('should alert mouseup on mouse up on the tr', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    component = shallow(<MessageList messages={messages} />);
+    const bodyMessages = component.find('tr').at(1);
+    bodyMessages.simulate('mouseup', { type: 'mouseup' });
+    expect(window.alert).toBeCalledWith('mouseup');
+  });
 });
--- 03 Tests samples/src/components/MessageList/MessageList.tsx
+++ 03 Tests samples/src/components/MessageList/MessageList.tsx
@@ -3,30 +3,33 @@ import './MessageList.less';
 import { Message } from '../../model';
 
 interface MessageListProps {
-    messages: Message[],
+  messages: Message[];
 }
 
 export const MessageList = (props: MessageListProps) => {
-	const [rowColor, setRowColor] = React.useState('white');
+	// const [rowColor, setRowColor] = React.useState('white');
 	const [clickPosition, setClickPosition] = React.useState({ clientX: 0, clientY: 0 });
 	const [showContextMenu, setShowContextMenu] = React.useState(false);
 
-	// const handleOnMouse = (event: React.MouseEvent) => {
-	// 	event.type === "mousedown" ? setRowColor('red') : setRowColor('blue');
-	// };
-	
-	const handleContextMenu = (event: React.MouseEvent) => {
+  const handleOnMouse = (event: React.MouseEvent) => {
+    event.type === 'mousedown' ? alert('mousedown') : alert('mouseup');
+  };
+
+  const handleContextMenu = (event: React.MouseEvent) => {
     setClickPosition({ clientX: event.clientX, clientY: event.clientY });
     event.preventDefault();
     document.addEventListener('mousedown', handleClickAfterContextMenuOpen);
     setShowContextMenu(true);
-	};
-	
-	const handleClickAfterContextMenuOpen = (e: MouseEvent) => {
+  };
+
+  const handleClickAfterContextMenuOpen = (e: MouseEvent) => {
     // Only if the left button is clicked - number 1
     // We need to perform this check since this method will also get called when right-clicking
     if (e.which === 1) {
-      document.removeEventListener('mousedown', handleClickAfterContextMenuOpen);
+      document.removeEventListener(
+        'mousedown',
+        handleClickAfterContextMenuOpen
+      );
       setTimeout(() => setShowContextMenu(false), 100);
     }
   };
@@ -40,13 +43,13 @@ export const MessageList = (props: MessageListProps) => {
 						<th>Body</th>
 					</tr>
 				</thead>
-				<tbody>{console.log('return', rowColor)}
+				<tbody>
 					{props.messages.map(message => (
-						<tr 
-							key={message.id} 
-							onMouseDown={() => setRowColor('red')} 
-							onMouseUp={() => setRowColor('blue')} 
-							style={{backgroundColor: rowColor}}>
+						<tr
+							key={message.id}
+							onMouseDown={handleOnMouse}
+							onMouseUp={handleOnMouse}
+					  	>
 							<td>{message.subject}</td>
 							<td>{message.body}</td>
 						</tr>
