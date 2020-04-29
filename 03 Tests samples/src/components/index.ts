110ab6fb12c1d2ab28a5a3c8123a80a4415dfac3
Aurora de Juan Ojeda <aurora.dejuan@blue-tc.com>
2020-04-29 09:18:34 +0200

    index-line

--- 03 Tests samples/src/components/index.ts
+++ 03 Tests samples/src/components/index.ts
@@ -3,7 +3,6 @@ import {MessageForm} from './MessageForm/MessageForm';
 import {MessagesSection} from './MessagesSection/MessagesSection';
 import {NameForm} from './NameForm/NameForm';
 
-
 export {
   MessageList,
   MessageForm,
