be8f2ee36d77aeaf488644eaaaa0dab74b037198
Aurora de Juan Ojeda <aurora.dejuan@blue-tc.com>
2020-04-29 09:16:57 +0200

    issue#21-some-comments

--- 03 Tests samples/src/components/NameForm/NameForm.test.tsx
+++ 03 Tests samples/src/components/NameForm/NameForm.test.tsx
@@ -5,20 +5,22 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 describe('<NameForm', () => {
-	configure({ adapter: new Adapter() });
-	it('Should call alert() when submit button is clicked', () => {
-		const component = mount(<NameForm />);
-		window.alert = jest.fn();
-		component.setState({ value: 'Hello' });
-		const expectedValue = 'A name was submitted: Hello';
-		component.find('form').simulate('submit');
-		expect(window.alert).toHaveBeenCalledWith(expectedValue);
-	});
-	it('Should handle state changes', () => {
-		const component = mount(<NameForm />);
-		component.setState({clicked: false});
-		expect(component.state().clicked).toBe(false);
-		component.find('form').simulate('submit');
-		expect(component.state().clicked).toBe(true);
-	});
+  configure({ adapter: new Adapter() });
+  let component;
+  beforeEach(() => {
+    component = mount(<NameForm />);
+  });
+  it('Should call alert() when submit button is clicked', () => {
+    window.alert = jest.fn();
+    component.setState({ value: 'Hello' });
+    const expectedValue = 'A name was submitted: Hello';
+    component.find('form').simulate('submit');
+    expect(window.alert).toHaveBeenCalledWith(expectedValue);
+  });
+  it('Should handle state changes', () => {
+    component.setState({clicked: false});
+    expect(component.state().clicked).toBe(false);
+    component.find('form').simulate('submit');
+    expect(component.state().clicked).toBe(true);
+  });
 });
--- 03 Tests samples/src/components/NameForm/NameForm.tsx
+++ 03 Tests samples/src/components/NameForm/NameForm.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
 
-
 interface MyState {
   value: string;
   clicked: boolean;
 }
 export class NameForm extends React.Component<{}, MyState> {
-	constructor(props: MyState) {
-		super(props);
-		this.state = { value: '' , clicked: false };
+  constructor(props: MyState) {
+    super(props);
+    this.state = { value: '' , clicked: false };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-	handleChange(event) {
-		this.setState({ value: event.target.value });
-	}
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    this.setState({clicked: true});
+    event.preventDefault();
+  }
 
-	handleSubmit(event) {
-		alert('A name was submitted: ' + this.state.value);
-		this.setState({clicked: true});
-		event.preventDefault();
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
           Name:
-					<input
-						type="text"
-						value={this.state.value}
-						onChange={this.handleChange}
-					/>
-				</label>
-				<input type="submit" value="Submit" />
-			</form>
-		);
-	}
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
